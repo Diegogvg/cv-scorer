c713c9b036eb00ac3bdb671644610519eca4718c
Diegogvg <vgdiego0112@gmail.com>
2026-08-10 20:17:17 -0500

    Update app.py
    err

--- app.py
+++ app.py
@@ -21,10 +21,11 @@ CERTIFICACIONES: SQL Server UNI-OTI, Excel Fundacion Romero, Fundamentos Analisi
 
 @app.route('/puntaje', methods=['POST'])
 def puntaje():
-    data = request.json
-    titulo = data.get('titulo', '')
-    empresa = data.get('empresa', '')
-    descripcion = data.get('descripcion', '')[:800]
+    try:
+        data = request.get_json(force=True, silent=True) or {}
+        titulo = str(data.get('titulo', ''))[:200]
+        empresa = str(data.get('empresa', ''))[:100]
+        descripcion = str(data.get('descripcion', ''))[:800]
     
     prompt = f"""Eres reclutador senior en Peru. Analiza que tan compatible es este candidato con la vacante.
 Responde SOLO con un numero entero del 1 al 100. Sin texto adicional, solo el numero.
